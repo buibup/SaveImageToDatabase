dc416e42a86149baebb47cf8aeef3a8f76dd05a1
Mongk <Mongk@DESKTOP-1TMBC4T>
2016-11-08 23:08:15 +0700

    Add project files.

---
+++ SaveImageToDatabase/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SaveImageToDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File name :";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(375, 285);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView.Location = new System.Drawing.Point(16, 327);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(1014, 224);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(353, 296);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(77, 311);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(13, 13);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "?";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(375, 256);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(375, 227);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "FIle name";
+            this.columnHeader1.Width = 504;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 563);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ SaveImageToDatabase/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveImageToDatabase
+{
+    public partial class Form1 : Form
+    {
+        private string _fileName;
+        private List<MyPicture> _list;
+
+        public Form1(List<MyPicture> list)
+        {
+            this._list = list;
+            InitializeComponent();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Image ConvertBinaryToImag(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView.FocusedItem != null)
+            {
+                pictureBox.Image = ConvertBinaryToImag(_list[listView.FocusedItem.Index].Data);
+                lblFileName.Text = listView.FocusedItem.SubItems[0].Text;
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (
+                OpenFileDialog ofd = new OpenFileDialog()
+                {
+                    Filter = @"JPEG|*.jpg",
+                    ValidateNames = true,
+                    Multiselect = false
+                })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    _fileName = ofd.FileName;
+                    lblFileName.Text = _fileName;
+                    pictureBox.Image = Image.FromFile(_fileName);
+                }
+            }
+        }
+
+        byte[] ConvertImageToBinary(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            using (PicEntities db = new PicEntities())
+            {
+                var pic = new MyPicture() { FileName = _fileName, Data = ConvertImageToBinary(pictureBox.Image) };
+                db.MyPictures.Add(pic);
+                try
+                {
+                    await db.SaveChangesAsync();
+                    MessageBox.Show(@"You have been successfully saved.", @"Message", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            System.Console.WriteLine(@"Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            listView.Items.Clear();
+            using (PicEntities db = new PicEntities())
+            {
+                _list = db.MyPictures.ToList();
+                foreach (var pic in _list)
+                {
+                    ListViewItem item = new ListViewItem(pic.FileName);
+                    listView.Items.Add(item);
+                }
+            }
+        }
+    }
+}
